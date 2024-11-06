2869ca4a2b41ccef7240d5e4cd9cbefa21620cc4
Punit Kumar <tagpunitkumar@gmail.com>
2024-11-06 19:09:36 +0530

    Add search functionality to ReportsDB component for patient name

--- src/components/ReportsDB.tsx
+++ src/components/ReportsDB.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { MoonLoader } from "react-spinners";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router-dom";
+import { Input } from "./ui/input";
 
 interface ReportData {
   patient_name: string;
@@ -21,6 +22,7 @@ interface ReportData {
 export default function ReportsDB() {
   const [reports, setReports] = React.useState<ReportData[]>([]);
   const [loading, setLoading] = React.useState(true);
+  const [query, setQuery] = React.useState("");
   const navigate = useNavigate();
 
   async function getReports() {
@@ -28,6 +30,19 @@ export default function ReportsDB() {
       .from("reportData")
       .select("*")
       .order("created_at", { ascending: false });
+    if (error) {
+      console.error(error);
+      setLoading(false);
+    }
+    setReports(data as ReportData[]);
+    setLoading(false);
+  }
+
+  async function searchResults() {
+    const { data, error } = await supabase
+      .from("reportData")
+      .select("*")
+      .like("patient_name", `%${query}%`);
     if (error) {
       console.error(error);
       setLoading(false);
@@ -47,80 +62,102 @@ export default function ReportsDB() {
       <div className="flex gap-2 items-center w-full bg-slate-700 text-white p-4 justify-center">
         <p className="font-semibold text-xl text-center">Report Database</p>
       </div>
-      <table className="border-2 w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th className="py-2 border-r border-gray-300 w-16">Sl. No.</th>
-            <th className="px-4 py-2 border-r border-gray-300">Patient Name</th>
-            <th className="px-4 py-2 border-r border-gray-300">
-              Reporting Date
-            </th>
-            <th className="px-4 py-2 border-r border-gray-300">
-              Collection Date
-            </th>
-            <th className="px-4 py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {loading && (
+      <div className="flex w-96 gap-2 justify-center items-center my-2 mx-auto">
+        <Input
+          placeholder="Search by patient name"
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              searchResults();
+            }
+          }}
+        />
+        <Button
+          onClick={() => {
+            searchResults();
+          }}
+        >
+          Search
+        </Button>
+      </div>
+      <div className="px-4">
+        <table className="border-2 w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
             <tr>
-              <td colSpan={5} className="text-center py-8">
-                <div className="flex justify-center items-center">
-                  <MoonLoader color="#000" />
-                </div>
-              </td>
+              <th className="py-2 border-r border-gray-300 w-16">Sl. No.</th>
+              <th className="px-4 py-2 border-r border-gray-300">
+                Patient Name
+              </th>
+              <th className="px-4 py-2 border-r border-gray-300">
+                Reporting Date
+              </th>
+              <th className="px-4 py-2 border-r border-gray-300">
+                Collection Date
+              </th>
+              <th className="px-4 py-2">Actions</th>
             </tr>
-          )}
-          {!reports.length && !loading && (
-            <tr>
-              <td colSpan={5} className="text-center py-4">
-                No reports found
-              </td>
-            </tr>
-          )}
-          {reports &&
-            !loading &&
-            reports.map((report, index) => (
-              <tr key={index}>
-                <td className="px-4 py-2 border-r text-center border-gray-300">
-                  {index + 1}
-                </td>
-                <td className="px-4 py-2 border-r border-gray-300">
-                  {report.patient_name}
-                </td>
-                <td className="px-4 py-2 border-r border-gray-300">
-                  {report.metadata.reportingDate}
-                </td>
-                <td className="px-4 py-2 border-r border-gray-300">
-                  {report.metadata.collectionDate}
-                </td>
-                <td className="px-4 py-2">
-                  <button
-                    className="bg-black text-white p-2 px-4 rounded-lg"
-                    onClick={() => {
-                      localStorage.removeItem("metadata");
-                      localStorage.removeItem("data");
-                      localStorage.setItem(
-                        "metadata",
-                        JSON.stringify(report.metadata)
-                      );
-                      localStorage.setItem(
-                        "data",
-                        JSON.stringify({
-                          selectedTestTypes: report.data.selectedTestTypes,
-                          reportData: report.data.reportData,
-                        })
-                      );
-                      window.open("/#/report", "_blank");
-                    }}
-                  >
-                    View
-                  </button>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {loading && (
+              <tr>
+                <td colSpan={5} className="text-center py-8">
+                  <div className="flex justify-center items-center">
+                    <MoonLoader color="#000" />
+                  </div>
                 </td>
               </tr>
-            ))}
-        </tbody>
-      </table>
+            )}
+            {!reports.length && !loading && (
+              <tr>
+                <td colSpan={5} className="text-center py-4">
+                  No reports found
+                </td>
+              </tr>
+            )}
+            {reports &&
+              !loading &&
+              reports.map((report, index) => (
+                <tr key={index}>
+                  <td className="px-4 py-2 border-r text-center border-gray-300">
+                    {index + 1}
+                  </td>
+                  <td className="px-4 py-2 border-r border-gray-300">
+                    {report.patient_name}
+                  </td>
+                  <td className="px-4 py-2 border-r border-gray-300">
+                    {report.metadata.reportingDate}
+                  </td>
+                  <td className="px-4 py-2 border-r border-gray-300">
+                    {report.metadata.collectionDate}
+                  </td>
+                  <td className="px-4 py-2">
+                    <button
+                      className="bg-black text-white p-2 px-4 rounded-lg"
+                      onClick={() => {
+                        localStorage.removeItem("metadata");
+                        localStorage.removeItem("data");
+                        localStorage.setItem(
+                          "metadata",
+                          JSON.stringify(report.metadata)
+                        );
+                        localStorage.setItem(
+                          "data",
+                          JSON.stringify({
+                            selectedTestTypes: report.data.selectedTestTypes,
+                            reportData: report.data.reportData,
+                          })
+                        );
+                        window.open("/#/report", "_blank");
+                      }}
+                    >
+                      View
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
